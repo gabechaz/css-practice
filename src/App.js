3f8cb5c93c6759eaaa1de0e2edec7479d2e9b1e1
Gabriel Chazanov <gabe.chaz@gmail.com>
2021-10-30 20:08:28 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+
+
+    <h1>Welcome to the landing page</h1>
+
+
+    
+
+
+
+
+
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
